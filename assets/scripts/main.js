9041485c0a52be1122e6b2128ecbdf98fea9af4b
andanfed <andanfe@gmail.com>
2020-05-03 11:24:00 +0300

    Add ryu animation moveset presentation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,12 @@
+const movesets = {
+  run: 'ryu-run.gif',
+  roundkick: 'ryu-leghit.gif',
+  hadouken: 'ryu-hadoken.gif',
+  uppercut: 'ryu-uppercut.gif',
+  stance: 'ryu-ts-stance.gif',
+  tie: 'ryu-tie.gif',
+};
+
 document.getElementById(
   'current-date'
 ).textContent = new Date().toLocaleDateString();
@@ -5,3 +14,23 @@ document.getElementById(
 document.getElementById('initial-date').textContent = new Date(
   0
 ).toLocaleDateString();
+
+const movesetButtons = Array.from(
+  document.getElementsByClassName('moveset-ctrl-btn')
+);
+
+const clearSelection = () => {
+  movesetButtons.forEach((btn) => btn.classList.remove('active'));
+};
+
+const handleBtnClick = (event) => {
+  event.preventDefault();
+  clearSelection();
+  event.target.classList.add('active');
+
+  document.getElementById('movement').src = `assets/images/moves/${
+    movesets[event.target.id]
+  }`;
+};
+
+movesetButtons.forEach((btn) => (btn.onclick = handleBtnClick));
